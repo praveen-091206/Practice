3e825c24d59826d1703d60bd15dcd4cbcec7c680
praveen-091206 <aditya.praveen1911@gmail.com>
2026-05-25 15:21:58 +0530

    Revert "Modified java program"
    This reverts commit 63637ffc4f5ba3f77d9691d3ab74a4b9ef65225a.

--- JAVAAA.java
+++ JAVAAA.java
@@ -1,5 +1,5 @@
 public class JAVAAA{
     public static void main(String[] args) {
-        System.out.println("Hello, World this is my first Java program!");
+        System.out.println("Hello, World!");
     }
 }
